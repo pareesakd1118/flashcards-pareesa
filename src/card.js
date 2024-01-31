1072122872541815af41eca9cca73c35bd97edd8
Pareesa Kamgar Dayhoff <pareesakd1118@gmail.com>
2024-01-31 15:55:39 -0800

    Completes iteration two

--- src/card.js
+++ src/card.js
@@ -7,14 +7,69 @@ function createCard(id, question, answers, correctAnswer) {
         correctAnswer
     }
 
-    return card
+    return card;
 }
 
-function evaluateGuess(guess, correctAnswer) {
-    return guess === correctAnswer.correctAnswer ? 'correct!' : 'incorrect!'
+function evaluateGuess(guess, card) {
+    return guess === card.correctAnswer ? 'correct!' : 'incorrect!';
 }
+
+function createDeck(deck) {
+    return deck;
+}
+
+function countDeck(deck) {
+    return deck.length;
+}
+
+function createRound(deck) {
+    let round = {
+        deck,
+        currentCard: deck[0],
+        turns: 0,
+        incorrectGuesses: [],
+    };
+
+    return round;
+}
+
+function takeTurn(guess, round) {
+    const evaluatedGuessMessage = evaluateGuess(guess, round.currentCard);
+
+    if (evaluatedGuessMessage === 'incorrect!') {
+        round.incorrectGuesses.push(round.currentCard.id)
+    }; 
+
+    round.turns++;
+    round.currentCard = round.deck[round.turns];
+
+    return evaluatedGuessMessage;
+}
+
+function calculatePercentCorrect(round) {
+    let percentCorrect = ((round.deck.length - round.incorrectGuesses.length)/round.deck.length) * 100;
+    percentCorrect = Math.round(percentCorrect);
+
+    return percentCorrect;
+}
+
+function endRound(round) {
+    let percentCorrect = ((round.deck.length - round.incorrectGuesses.length)/round.deck.length) * 100;
+    percentCorrect = Math.round(percentCorrect);
+    let endMessage = `** Round over! ** You answered ${percentCorrect}% of the questions correctly!`
+
+    console.log(endMessage);
+}
+
+
 
 module.exports = { 
     createCard,
-    evaluateGuess
+    evaluateGuess,
+    createDeck,
+    countDeck,
+    createRound,
+    takeTurn,
+    calculatePercentCorrect,
+    endRound
 };
--- test/card-test.js
+++ test/card-test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { createCard, evaluateGuess } = require('../src/card');
+const { createCard, evaluateGuess, createDeck, countDeck, createRound, takeTurn, calculatePercentCorrect, endRound } = require('../src/card');
 
 describe('card', function() {
   it('should be a function', function() {
@@ -29,4 +29,204 @@ describe('card', function() {
     expect(turn1).to.equal('correct!');
     expect(turn2).to.equal('incorrect!');
   });  
+
+  it('should be a function', function() {
+    expect(createDeck).to.be.a('function');
+  });
+
+  it('should create a deck of cards', function() {
+    const card1 = createCard(1, 'What is Pareesas favorite color?', ['blue', 'green', 'pink'], 'blue');
+    const card2 = createCard(2, 'What is Obisidians favorite snack?', ['beef jerkey', 'cherry tomatoes', 'chicken bones'], 'cherry tomatoes');
+    const card3 = createCard(3, 'What state only borders one other state?', ['Florida', 'Maryland', 'Maine'], 'Maine');
+
+    const deck = createDeck([card1, card2, card3])
+
+    expect(deck).to.deep.equal([card1, card2, card3]);
+  });  
+
+  it('should be a function', function() {
+    expect(countDeck).to.be.a('function');
+  });
+
+  it('should count how many cards are in the deck', function() {
+    const card1 = createCard(1, 'What is Pareesas favorite color?', ['blue', 'green', 'pink'], 'blue');
+    const card2 = createCard(2, 'What is Obisidians favorite snack?', ['beef jerkey', 'cherry tomatoes', 'chicken bones'], 'cherry tomatoes');
+    const card3 = createCard(3, 'What state only borders one other state?', ['Florida', 'Maryland', 'Maine'], 'Maine');
+
+    const deck = createDeck([card1, card2, card3])
+    const deckCount = countDeck(deck)
+
+    expect(deckCount).to.equal(3);
+  }); 
+  
+  it('should be a function', function() {
+    expect(createRound).to.be.a('function');
+  });
+
+  it('should create a round with a property of deck which holds the array of cards object', function() {
+    const card1 = createCard(1, 'What is Pareesas favorite color?', ['blue', 'green', 'pink'], 'blue');
+    const card2 = createCard(2, 'What is Obisidians favorite snack?', ['beef jerkey', 'cherry tomatoes', 'chicken bones'], 'cherry tomatoes');
+    const card3 = createCard(3, 'What state only borders one other state?', ['Florida', 'Maryland', 'Maine'], 'Maine');
+
+    const deck = [card1, card2, card3]
+    const round = createRound(deck)
+
+    expect(round.deck).to.equal(deck);
+    expect(round.deck.length).to.equal(3);
+  }); 
+
+  it('should have the first card in the deck as the currentCard property value', function() {
+    const card1 = createCard(1, 'What is Pareesas favorite color?', ['blue', 'green', 'pink'], 'blue');
+    const card2 = createCard(2, 'What is Obisidians favorite snack?', ['beef jerkey', 'cherry tomatoes', 'chicken bones'], 'cherry tomatoes');
+    const card3 = createCard(3, 'What state only borders one other state?', ['Florida', 'Maryland', 'Maine'], 'Maine');
+
+    const deck = [card1, card2, card3]
+    const round = createRound(deck)
+
+    expect(round.currentCard).to.equal(deck[0]);
+  }); 
+
+  it('should have a turns propety whose value starts as 0', function() {
+    const card1 = createCard(1, 'What is Pareesas favorite color?', ['blue', 'green', 'pink'], 'blue');
+    const card2 = createCard(2, 'What is Obisidians favorite snack?', ['beef jerkey', 'cherry tomatoes', 'chicken bones'], 'cherry tomatoes');
+    const card3 = createCard(3, 'What state only borders one other state?', ['Florida', 'Maryland', 'Maine'], 'Maine');
+
+    const deck = [card1, card2, card3]
+    const round = createRound(deck)
+
+    expect(round.turns).to.equal(0);
+  }); 
+
+  it('should have an incorrectGuesses property that starts as empty array that stores incorrect guesses', function() {
+    const card1 = createCard(1, 'What is Pareesas favorite color?', ['blue', 'green', 'pink'], 'blue');
+    const card2 = createCard(2, 'What is Obisidians favorite snack?', ['beef jerkey', 'cherry tomatoes', 'chicken bones'], 'cherry tomatoes');
+    const card3 = createCard(3, 'What state only borders one other state?', ['Florida', 'Maryland', 'Maine'], 'Maine');
+
+    const deck = [card1, card2, card3]
+    const round = createRound(deck)
+
+    expect(round.incorrectGuesses).to.deep.equal([]);
+    expect(round.incorrectGuesses).to.have.lengthOf(0);
+  }); 
+
+  it('should be a function', function() {
+    expect(takeTurn).to.be.a('function');
+  });
+
+  it('should increase the turns property by 1 regardless of whether the guess was correct', function() {
+    const card1 = createCard(1, 'What is Pareesas favorite color?', ['blue', 'green', 'pink'], 'blue');
+    const card2 = createCard(2, 'What is Obisidians favorite snack?', ['beef jerkey', 'cherry tomatoes', 'chicken bones'], 'cherry tomatoes');
+    const card3 = createCard(3, 'What state only borders one other state?', ['Florida', 'Maryland', 'Maine'], 'Maine');
+
+    const deck = [card1, card2, card3]
+    const round = createRound(deck)
+    
+    expect(round.turns).to.equal(0); 
+    const turn1 = takeTurn('blue', round)
+    expect(round.turns).to.equal(1); 
+    const turn2 = takeTurn('beef jerkey', round)
+    expect(round.turns).to.equal(2); 
+    const turn3 = takeTurn('Maine', round)   
+  });
+
+  it('should change the currentCard property to the next card in the deck', function() {
+    const card1 = createCard(1, 'What is Pareesas favorite color?', ['blue', 'green', 'pink'], 'blue');
+    const card2 = createCard(2, 'What is Obisidians favorite snack?', ['beef jerkey', 'cherry tomatoes', 'chicken bones'], 'cherry tomatoes');
+    const card3 = createCard(3, 'What state only borders one other state?', ['Florida', 'Maryland', 'Maine'], 'Maine');
+
+    const deck = [card1, card2, card3]
+    const round = createRound(deck)
+
+    expect(round.currentCard).to.equal(deck[0]); 
+
+    const turn1 = takeTurn('blue', round)
+
+    expect(round.currentCard).to.equal(deck[1]); 
+
+    const turn2 = takeTurn('beef jerkey', round)
+
+    expect(round.currentCard).to.equal(deck[2]); 
+
+    const turn3 = takeTurn('Maine', round)
+  });
+
+  it('should store incorrect guesses by ID in the incorrectGuesses property', function() {
+    const card1 = createCard(1, 'What is Pareesas favorite color?', ['blue', 'green', 'pink'], 'blue');
+    const card2 = createCard(2, 'What is Obisidians favorite snack?', ['beef jerkey', 'cherry tomatoes', 'chicken bones'], 'cherry tomatoes');
+    const card3 = createCard(3, 'What state only borders one other state?', ['Florida', 'Maryland', 'Maine'], 'Maine');
+
+    const deck = [card1, card2, card3]
+    const round = createRound(deck)
+
+    const turn1 = takeTurn('blue', round)
+
+    expect(round.incorrectGuesses).to.deep.equal([]); 
+
+    const turn2 = takeTurn('beef jerkey', round)
+
+    expect(round.incorrectGuesses).to.deep.equal([2]);
+
+    const turn3 = takeTurn('Maine', round)
+
+    expect(round.incorrectGuesses).to.deep.equal([2]); 
+  });
+
+  it('should evaluate the guess and return a message saying whether it was correct or incorrect', function() {
+    const card1 = createCard(1, 'What is Pareesas favorite color?', ['blue', 'green', 'pink'], 'blue');
+    const card2 = createCard(2, 'What is Obisidians favorite snack?', ['beef jerkey', 'cherry tomatoes', 'chicken bones'], 'cherry tomatoes');
+    const card3 = createCard(3, 'What state only borders one other state?', ['Florida', 'Maryland', 'Maine'], 'Maine');
+
+    const deck = [card1, card2, card3]
+    const round = createRound(deck)
+    const turn1 = takeTurn('blue', round)
+    const turn2 = takeTurn('beef jerkey', round)
+    const turn3 = takeTurn('Maine', round)
+
+    expect(turn1).to.deep.equal('correct!'); 
+    expect(turn2).to.deep.equal('incorrect!'); 
+    expect(turn3).to.deep.equal('correct!'); 
+  });
+//--------------------------------------------------------------------
+  it('should be a function', function() {
+    expect(calculatePercentCorrect).to.be.a('function');
+  });
+
+  it('calculates and returns the percentage of correct guesses', function() {
+    const card1 = createCard(1, 'What is Pareesas favorite color?', ['blue', 'green', 'pink'], 'blue');
+    const card2 = createCard(2, 'What is Obisidians favorite snack?', ['beef jerkey', 'cherry tomatoes', 'chicken bones'], 'cherry tomatoes');
+    const card3 = createCard(3, 'What state only borders one other state?', ['Florida', 'Maryland', 'Maine'], 'Maine');
+
+    const deck = [card1, card2, card3]
+    const round = createRound(deck)
+    
+    const turn1 = takeTurn('blue', round)
+    const turn2 = takeTurn('beef jerkey', round)
+    const turn3 = takeTurn('Maine', round) 
+    
+    const percentCorrect = calculatePercentCorrect(round)
+
+    expect(percentCorrect).to.equal(67);
+  });
+
+  it('should be a function', function() {
+    expect(endRound).to.be.a('function');
+  });
+
+  it('prints the following to the console: ‘** Round over! ** You answered <>% of the questions correctly!', function() {
+    const card1 = createCard(1, 'What is Pareesas favorite color?', ['blue', 'green', 'pink'], 'blue');
+    const card2 = createCard(2, 'What is Obisidians favorite snack?', ['beef jerkey', 'cherry tomatoes', 'chicken bones'], 'cherry tomatoes');
+    const card3 = createCard(3, 'What state only borders one other state?', ['Florida', 'Maryland', 'Maine'], 'Maine');
+
+    const deck = [card1, card2, card3]
+    const round = createRound(deck)
+    
+    const turn1 = takeTurn('blue', round)
+    const turn2 = takeTurn('beef jerkey', round)
+    const turn3 = takeTurn('Maine', round) 
+    
+    const endRoundMessage = endRound(round)
+
+    expect(endRoundMessage).to.equal(undefined);
+  });
 });
+
